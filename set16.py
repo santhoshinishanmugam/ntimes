c8073cbd9de5610a6f82f0c59c6440dae41deca2
santhoshinishanmugam <37971016+santhoshinishanmugam@users.noreply.github.com>
2019-07-04 11:13:30 +0530

    Update set16.py

--- set16.py
+++ set16.py
@@ -1,2 +1,14 @@
-n,i=input().split()
-
+n,t=input().split()
+s=int(n)
+g=int(t)
+for i in range(s+1,g+1):
+  if(i>1 and i<g):
+    for n in range (2,i):
+      if((i%n))==0):
+        break
+      else:
+        print(i,end=" ")
+       
+          
+         
+             
