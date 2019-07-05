6043f84adb9decd8114901a4bcc3bc2a0dd54989
santhoshinishanmugam <37971016+santhoshinishanmugam@users.noreply.github.com>
2019-07-05 14:30:39 +0530

    Update set40.py

--- set40.py
+++ set40.py
@@ -5,9 +5,9 @@ count=0
 if(input =='0'):
   print("0")
 while(count<input):
-  print(s1, end=' '):
-    s3=s1+s2
-    s1=s2
-    s2=s3
-    count+=1
+  print(s1, end=' ')
+  s3=s1+s2
+  s1=s2
+  s2=s3
+  count+=1
 
