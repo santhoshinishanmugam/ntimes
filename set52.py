7c48f0a35c2dfa7ae60e3dd31821ef41bd4d5eb9
santhoshinishanmugam <37971016+santhoshinishanmugam@users.noreply.github.com>
2019-07-06 11:06:24 +0530

    Update set52.py

--- set52.py
+++ set52.py
@@ -1,1 +1,15 @@
-
+thisdict = {
+  1: "One",
+  2: "Two",
+  3: "Three",
+  4: "Four",
+  5: "Five",
+  6: "Six",
+  7: "Seven",
+  8: "Eight",
+  9: "Nine",
+ 10: "Ten"
+}
+c=int(input())
+s = thisdict.get(c)
+print(s)
