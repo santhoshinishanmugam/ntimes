f73e802b082ad8c9c2ac2f2795bf7da8fb111e41
santhoshinishanmugam <37971016+santhoshinishanmugam@users.noreply.github.com>
2019-07-07 19:48:00 +0530

    Update setp9.py

--- setp9.py
+++ setp9.py
@@ -1,10 +1,12 @@
-inp1,inp2=map(int,input().split())
-count=0
-for n in range(inp1,inp2+1):
-  if(n > 1):
-    for x in range(2,n):
-      if((n%x)==0):
+s1,s2=map(int,input().spplit())
+s3=0
+for i in range(s1,s2+1):
+  if i>1:
+    for x in range(2,i):
+      if x%2==0:
         break
-      else:
-        count=count+1
-print(count)        
+  else:
+    s3+=1
+print(s3)    
+  
+          
