75887d992e05c2f0a5b729856f9cead8655b4f62
santhoshinishanmugam <37971016+santhoshinishanmugam@users.noreply.github.com>
2019-07-07 19:27:35 +0530

    Update setp9.py

--- setp9.py
+++ setp9.py
@@ -1,1 +1,10 @@
-
+c,v=map(int,input().split())
+p=[]
+for j in range(c+1,v+1):
+  if j>1:
+    for i in range(2,j):
+      if(j%v==0):
+        break
+      else:
+        p.append(i)
+  print(len(p)+1)      
