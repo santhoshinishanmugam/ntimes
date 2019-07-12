840d21b060e75d8b0e502ad83e4e7ee74d81344d
santhoshinishanmugam <37971016+santhoshinishanmugam@users.noreply.github.com>
2019-07-12 22:05:26 +0530

    Update setppro1.py

--- setppro1.py
+++ setppro1.py
@@ -2,7 +2,7 @@ def l(str1,str2):
   if(str1 in str2):
     return str1
   else:
-    retur l(str[0:len(str1)-1],str2)
+    return l(str[0:len(str1)-1],str2)
 s=int(input())
 m=[]
 for_in range(0,s):
