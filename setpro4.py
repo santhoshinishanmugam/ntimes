d3963c5398d6bca1d33e089eb051ab1e53720621
santhoshinishanmugam <37971016+santhoshinishanmugam@users.noreply.github.com>
2019-07-13 19:31:58 +0530

    Update setpro4.py

--- setpro4.py
+++ setpro4.py
@@ -1,2 +1,11 @@
 s1,s2=map(str,input().slit())
 san=0
+if len(s1)>len(s2):
+   s1,s2=s2,s1
+p=0
+while p<len(s1):
+    san+=(ord(s2[p]-ord(s1[p]))
+    p+=1
+  for p in range(p,len(s2)):
+    san+=ord(s2[p]-ord('a')+1
+  print(san)
