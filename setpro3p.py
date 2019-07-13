d1703742bad01220b03311338007085eae73dc3a
santhoshinishanmugam <37971016+santhoshinishanmugam@users.noreply.github.com>
2019-07-13 19:02:58 +0530

    Update setpro3p.py

--- setpro3p.py
+++ setpro3p.py
@@ -1,1 +1,11 @@
+strr1,strr2=input().split()
+length=len(strr1) if len(strr1)<len(strr2) else len(strr2)
+diff=abs(len(strr1)-len(strr2))
+counts=diff
+for i in range(length):
+  if(len(strr2)==1 and strr2[i] in strr1):
+    break
+  if(strr1[i]!=strr2[i]):
+    counts+=1
+print(counts)    
 
