12a1806277fe2f9cb3e061f530954bae6524a7e0
santhoshinishanmugam <37971016+santhoshinishanmugam@users.noreply.github.com>
2019-07-13 19:39:49 +0530

    Update setpro4p.py

--- setpro4p.py
+++ setpro4p.py
@@ -4,8 +4,7 @@ if len(s1)>len(s2):
    s1,s2=s2,s1
 p=0
 while p<len(s1):
-    san+=(ord(s2[p]-ord(s1[p]))
-    p+=1
-  for p in range(p,len(s2)):
-    san+=ord(s2[p]-ord('a')+1
-  print(san)
+   san+=(ord(s2[p]-ord(s1[p]))
+   p+=1
+for p in range(p,len(s2)):
+         san+=ord(s2[p]-ord('a')+1
