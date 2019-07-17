244cfb2b57a3919e0b7ada4f79f07bfaed930876
santhoshinishanmugam <37971016+santhoshinishanmugam@users.noreply.github.com>
2019-07-17 11:17:58 +0530

    Update sethunt8.py

--- sethunt8.py
+++ sethunt8.py
@@ -1,1 +1,11 @@
-
+from itertools import permutations
+s=input()
+a=list(map(int,input().split()))
+y=[]
+for i in permutations(x,3):
+  if i[0] < i[1] < i[2]:
+    if i[0] + i[1] == i[3]:
+      y.append([str[j] for j in i])
+for i in r:
+  print(' '.join(i))
+      
