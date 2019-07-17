ed5763f62849c38ab878db8298f0c5b23f38344e
santhoshinishanmugam <37971016+santhoshinishanmugam@users.noreply.github.com>
2019-07-17 10:14:28 +0530

    Update setds.py

--- setds.py
+++ setds.py
@@ -1,1 +1,27 @@
+class stack:
+	def __init__(self,size=0):
+		self.size=size
+		self.list1=[]
+	def add(self,data):
+		if len(self.list1)==self.size:
+			print("Stack is full")
+			return
+		self.list1.append(data)
+		
+	def remove(self):
+		if len(self.list1)==0:
+			print("Stack is empty")
+			return
+		self.list1.pop()
+a=stack(3)
+a.add(1)
+print(a.list1)
+a.add(2)
+print(a.list1)
+a.add(3)
+print(a.list1)
+a.remove()
+print(a.list1)
 
+		
+		
