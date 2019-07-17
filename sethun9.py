eca3d6083c6010f8bc891e6477b4c471b02b51c4
santhoshinishanmugam <37971016+santhoshinishanmugam@users.noreply.github.com>
2019-07-17 11:38:40 +0530

    Update sethun9.py

--- sethun9.py
+++ sethun9.py
@@ -1,1 +1,15 @@
+from itertools import permutations
+s = input()
+a = list(map(int,input().split()))
+t = 0
+h = 10000
+for i in permutations(a,2):
+  if sum(i) == 0:
+    t = i
+    break
+  elif 0-sum(i) < h:
+    h = 0-sum(i)
+    t = i
+print(t[0],t[1])    
+    
 
