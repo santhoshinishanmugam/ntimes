33ff2e328ab9a8de95511a8ecb31760eb274b0ab
santhoshinishanmugam <37971016+santhoshinishanmugam@users.noreply.github.com>
2019-07-17 11:27:09 +0530

    Update sethunt8.py

--- sethunt8.py
+++ sethunt8.py
@@ -1,7 +1,7 @@
 from itertools import permutations
-s=input()
-a=list(map(int,input().split()))
-y=[]
+s = input()
+a = list(map(int,input().split()))
+y = []
 for i in permutations(a,3):
   if i[0] < i[1] < i[2]:
     if i[0] + i[1] == i[3]:
