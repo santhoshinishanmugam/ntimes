1c5953482f70f2b256378a2aaad9df2389d04a9e
santhoshinishanmugam <37971016+santhoshinishanmugam@users.noreply.github.com>
2019-07-17 10:16:27 +0530

    Update sethunt7.py

--- sethunt7.py
+++ sethunt7.py
@@ -2,8 +2,8 @@ s=input()
 a=list(map(int,input().split()))
 t=[]
 for i in range(len(a)):
-  if i%2==0:
-    if a[i]%2!=0:
+  if i % 2 == 0:
+    if a[i] % 2 !=0:
       t.append(a[i])
 for i in t:
   print(i,end=" ")
