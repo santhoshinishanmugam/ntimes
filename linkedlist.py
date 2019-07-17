c05458b92c401f8fffe336abcfee2939d9f9c5fc
santhoshinishanmugam <37971016+santhoshinishanmugam@users.noreply.github.com>
2019-07-17 14:07:27 +0530

    Update linkedlist.py

--- linkedlist.py
+++ linkedlist.py
@@ -1,1 +1,25 @@
-
+class node:
+	def __init__(self,data):
+		self.data=data
+		self.next=None
+class LinkedList:
+	def __init__(self):
+		self.head=None
+	def printList(self):
+		temp = self.head
+		while temp:
+			print(temp.data)
+			temp=temp.next
+    def insert(self,data):
+    	newnode = Node(data)
+    	newnode.next=self.head
+    	self.head=newnode
+if__name__=='__main__':
+	list = LinkedList()
+	list.head=Node(1)
+	second = Node(2)
+	third  = Node(3)
+	list.head.next = second
+	second.next=third
+	list.printList()
+		
