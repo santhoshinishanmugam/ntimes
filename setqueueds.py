2a0989108f8f98c9308766266ef8dfb5f179f7e6
santhoshinishanmugam <37971016+santhoshinishanmugam@users.noreply.github.com>
2019-07-17 10:48:05 +0530

    Update setqueueds.py

--- setqueueds.py
+++ setqueueds.py
@@ -1,1 +1,22 @@
+class Queue:
+	def __init__(self,size=0):
+		self.size=size
+		self.list1=[]
+	def add(self,data):
+		if len(self.list1)>=self.size:
+			print("queue is empty")
+		self.list1.append(data)	
+	def remove(self):
+		if len(self.list1)<=0:
+			print("queue is empty")
+			return
+		self.list1.pop(0)
+obj=Queue(3)
+obj.add(5)
+print(obj.list1)
+obj.add(6)
+print(obj.list1)
+obj.add(7)
+print(obj.list1)
 
+	
