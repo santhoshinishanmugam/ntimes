628d742911d9870b4ac34c12f14bf808ed0f61c8
santhoshinishanmugam <37971016+santhoshinishanmugam@users.noreply.github.com>
2019-07-20 16:08:27 +0530

    Update sethunt13.py

--- sethunt13.py
+++ sethunt13.py
@@ -1,9 +1,8 @@
-s = [i for i in input()]
-a = s[::-1]
-while lens(s) > 1:
-  if s.pop(0)!= s.pop():
-    print('NO')
+s = input()
+a=x[::-1]
+if(a==x):
+  print('NO')
     break
-  else:
-    print('YES')
+else:
+  print('YES')
   
