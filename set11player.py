76ee8617eeb75dc6773f3af1493bc661cb94db36
santhoshinishanmugam <37971016+santhoshinishanmugam@users.noreply.github.com>
2019-08-07 20:17:10 +0530

    Update set11player.py

--- set11player.py
+++ set11player.py
@@ -1,7 +1,5 @@
 days = [ 'Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
-a = days[0]
-s = days[6]
-if a and s:
+if days[0] and days[6]:
   print("yes")
 else:
   print("no")
