5f9b5a7d123aeb5058e5eb5c702e4aeab707b3e0
santhoshinishanmugam <37971016+santhoshinishanmugam@users.noreply.github.com>
2019-08-07 19:21:44 +0530

    Update set11player.py

--- set11player.py
+++ set11player.py
@@ -1,4 +1,4 @@
-days = [ 'sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday']
+days = [ 'Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
 a = days[0]
 s = days[6]
 if a and s:
